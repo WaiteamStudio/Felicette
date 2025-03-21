65f38e4f76b5ba45e9f170954e12660e08518c86
Barsik2000 <orlov.serg2000@gmail.com>
2025-03-21 13:41:56 +0300

    Telescope

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -1,14 +1,12 @@
 using DialogueEditor;
 using UnityEngine;
 
-
-public class PlayerMovement : MonoBehaviour, IMovement
+public class PlayerMovement : MonoBehaviour, IMovement, IService
 {
     [SerializeField] private float speed;
     private bool isMovementEnabled = true;
     private Vector2 followSpot;
     private UnityEngine.AI.NavMeshAgent agent;
-    private Animator animator;
 
     public Vector2 velocity => new Vector2(agent.velocity.x, agent.velocity.y); //Публичное свойство для получения скорости
 
@@ -18,7 +16,6 @@ public class PlayerMovement : MonoBehaviour, IMovement
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
-        animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -31,8 +28,6 @@ public class PlayerMovement : MonoBehaviour, IMovement
         {
             agent.isStopped = false;
         }
-
-        FeliAnimateBasic();
     }
 
     public void UpdateFollowSpot(Vector2 newSpot)
@@ -46,18 +41,5 @@ public class PlayerMovement : MonoBehaviour, IMovement
     public void Teleport(Vector3 position)
     {
         agent.Warp(position);
-    }
-
-    private void FeliAnimateBasic()
-    {
-        animator.SetInteger("horizontal speed", (int)(velocity.x*10));
-        animator.SetInteger("vertical speed", (int)(velocity.y * 10));
-       
-        if(Mathf.Abs(velocity.y)>1.55* Mathf.Abs(velocity.x)) animator.SetInteger("horizontal speed", 0);
-        if (Mathf.Abs(velocity.x) > 1.55 * Mathf.Abs(velocity.y)) animator.SetInteger("vertical speed", 0);
-
-        if (Mathf.Abs(velocity.x) < 0.15) animator.SetInteger("horizontal speed", 0);
-        if (Mathf.Abs(velocity.y) < 0.15) animator.SetInteger("vertical speed", 0);
-
     }
 }
